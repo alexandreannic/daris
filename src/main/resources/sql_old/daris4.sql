ac7f75b9207dd8feb9398a66632aff52b350f8b1
Alexandre Annic <alexandre.annic1@gmail.com>
2015-11-12 23:09:51 +0100

    add .sql

--- src/main/resources/sql_old/daris4.sql
+++ src/main/resources/sql_old/daris4.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 12 Novembre 2015 à 23:09
+-- Généré le :  Dim 08 Novembre 2015 à 00:30
 -- Version du serveur :  5.6.24
 -- Version de PHP :  5.6.8
 
@@ -29,23 +29,18 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `activity` (
   `id` bigint(20) NOT NULL,
   `locality_id` bigint(20) NOT NULL,
-  `date_from` varchar(10) DEFAULT NULL,
-  `date_to` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+  `from` date NOT NULL,
+  `to` date NOT NULL,
+  `event_id` bigint(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `activity`
 --
 
-INSERT INTO `activity` (`id`, `locality_id`, `date_from`, `date_to`) VALUES
-(25, 2, '2', ''),
-(26, 1, '1', ''),
-(27, 2, '2', ''),
-(28, 4, '04:00', '17:30'),
-(29, 2, '04:00', '23:30'),
-(30, 7, '04:00', '17:45'),
-(31, 4, '03:00', '17:45'),
-(32, 1, '1', '');
+INSERT INTO `activity` (`id`, `locality_id`, `from`, `to`, `event_id`) VALUES
+(1, 1, '2015-11-05', '2015-11-19', 1),
+(2, 1, '2015-11-25', '2015-11-28', 1);
 
 -- --------------------------------------------------------
 
@@ -57,59 +52,16 @@ CREATE TABLE IF NOT EXISTS `event` (
   `id` bigint(20) NOT NULL,
   `name` varchar(255) NOT NULL,
   `user_id` bigint(20) NOT NULL,
-  `day` date NOT NULL,
-  `picture` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `max_user` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+  `picture` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `event`
 --
 
-INSERT INTO `event` (`id`, `name`, `user_id`, `day`, `picture`, `description`, `max_user`) VALUES
-(23, 'Un super événement !', 12, '2015-11-28', NULL, '', 0),
-(24, 'Sortie romantique !', 12, '2015-11-10', NULL, '', 0),
-(25, 'Promenade au clair de lune', 12, '2015-11-22', NULL, '', 0),
-(26, 'Un bol d''air', 12, '2015-11-14', NULL, 'Prenons un bol !', 0),
-(27, 'Un événement', 12, '2015-11-02', NULL, '', 0),
-(28, 'Il faut se cultiver !', 12, '2015-12-18', NULL, '', 0);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `event_activities`
---
-
-CREATE TABLE IF NOT EXISTS `event_activities` (
-  `event_id` bigint(20) NOT NULL,
-  `activities_id` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `event_activity`
---
-
-CREATE TABLE IF NOT EXISTS `event_activity` (
-  `event_id` bigint(20) NOT NULL,
-  `activities_id` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `event_activity`
---
-
-INSERT INTO `event_activity` (`event_id`, `activities_id`) VALUES
-(23, 25),
-(23, 26),
-(24, 27),
-(25, 28),
-(25, 29),
-(26, 30),
-(27, 31),
-(28, 32);
+INSERT INTO `event` (`id`, `name`, `user_id`, `picture`, `description`) VALUES
+(1, 'super sortie', 4, 'louvre.jpg', '');
 
 -- --------------------------------------------------------
 
@@ -119,31 +71,8 @@ INSERT INTO `event_activity` (`event_id`, `activities_id`) VALUES
 
 CREATE TABLE IF NOT EXISTS `event_participant` (
   `event_id` bigint(20) NOT NULL,
-  `participants_id` bigint(20) NOT NULL
+  `user_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `event_participant`
---
-
-INSERT INTO `event_participant` (`event_id`, `participants_id`) VALUES
-(15, 4),
-(16, 4),
-(17, 4),
-(18, 4),
-(21, 4),
-(22, 9),
-(23, 9),
-(23, 12),
-(23, 13),
-(24, 4),
-(24, 11),
-(25, 9),
-(25, 12),
-(26, 4),
-(26, 10),
-(27, 11),
-(28, 9);
 
 -- --------------------------------------------------------
 
@@ -159,21 +88,19 @@ CREATE TABLE IF NOT EXISTS `locality` (
   `picture` varchar(255) DEFAULT NULL,
   `type` varchar(255) NOT NULL,
   `description` text,
+  `note` int(5) DEFAULT NULL,
   `price` varchar(255) NOT NULL,
-  `note` int(5) NOT NULL,
-  `latitude` float(9,7) DEFAULT NULL,
-  `longetude` float(9,7) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `latitude` int(255) DEFAULT NULL,
+  `longetude` int(255) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `locality`
 --
 
-INSERT INTO `locality` (`id`, `name`, `address`, `ZIPCode`, `picture`, `type`, `description`, `price`, `note`, `latitude`, `longetude`) VALUES
-(1, 'Musée du Louvre', 'rue du Louvre', '75001', 'louvre.jpg', 'Musée', '', '', 0, 48.8591995, 2.3417001),
-(2, 'Arc-de-Triomphe', 'Place Charles de Gaulle', '75008', 'arc.jpg', 'Monument', '', '', 0, 48.8737564, 2.2949460),
-(4, 'Tour Eiffel', '5 Avenue Anatole France', '75007', 'eiffel.jpg', 'Monument', 'a tour Eiffel est une tour de fer puddlé de 324 mètres de hauteur (avec antennes) située à Paris, à l''extrémité nord-ouest du parc du Champ-de-Mars.', '', 0, 48.8588448, 2.2943511),
-(7, 'Sacré coeur', '35 Rue du Chevalier de la Barre', '75018', 'sacrecoeur.jpg', 'Musée', 'La basilique du Sacré-C?ur, dite du V?u national, située au sommet de la butte Montmartre, dans le 18&#7497; arrondissement de Paris, est un édifice religieux parisien majeur. Sa construction suit l''après-guerre de 1870.', '', 0, 48.8866310, 2.3429284);
+INSERT INTO `locality` (`id`, `name`, `address`, `ZIPCode`, `picture`, `type`, `description`, `note`, `price`, `latitude`, `longetude`) VALUES
+(1, 'Louvre', '12 rue du Louvre', '75010', 'louvre.jpg', 'Musée', 'Le musée du Louvre est un musée d''art et d''antiquités situé au centre de Paris dans le palais du Louvre', 4, '15 euros', 300, 300),
+(13, 'centre21', '18 rue chamberlin', '91600', '', 'Musée', '', NULL, '', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -209,6 +136,16 @@ CREATE TABLE IF NOT EXISTS `message_events` (
   `message_id` bigint(20) NOT NULL,
   `event_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `message_events`
+--
+
+INSERT INTO `message_events` (`message_id`, `event_id`) VALUES
+(18, 1),
+(20, 1),
+(21, 1),
+(22, 1);
 
 -- --------------------------------------------------------
 
@@ -253,7 +190,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `ZIPCode` varchar(255) DEFAULT NULL,
   `picture` varchar(255) DEFAULT NULL,
   `inscription` date NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 --
 -- Contenu de la table `user`
@@ -264,8 +201,7 @@ INSERT INTO `user` (`id`, `email`, `password`, `firstName`, `lastName`, `city`, 
 (9, 'feafa@afre.fr', 'e2ad69de782095a125667ef33d201eb4e8054149', 'Karl', 'Marx', NULL, NULL, NULL, NULL, '2015-11-01'),
 (10, 'eeee@eee.fr', 'e2ad69de782095a125667ef33d201eb4e8054149', 'Trung', 'Nguyen', NULL, NULL, NULL, NULL, '2015-11-18'),
 (11, 'ioio@ioio.fr', '993ca2a41505bd01a520a3af02783ec1bd687031', 'Brithney', 'Spears', NULL, NULL, NULL, NULL, '2015-11-23'),
-(12, 'aaa@aaa.fr', 'e2ad69de782095a125667ef33d201eb4e8054149', 'Alexandre', 'Annic', NULL, NULL, NULL, 'p1.jpg', '2015-11-10'),
-(13, 'bbb@bbb.fr', 'e2ad69de782095a125667ef33d201eb4e8054149', 'aze', 'Annic', NULL, NULL, NULL, NULL, '2015-11-11');
+(12, 'aaa@aaa.fr', 'e2ad69de782095a125667ef33d201eb4e8054149', 'Alexandre', 'Annic', NULL, NULL, NULL, 'p1.jpg', '2015-11-10');
 
 -- --------------------------------------------------------
 
@@ -294,7 +230,7 @@ INSERT INTO `user_relation` (`user_id`, `followed_id`) VALUES
 -- Index pour la table `activity`
 --
 ALTER TABLE `activity`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`), ADD KEY `fk_event` (`event_id`);
 
 --
 -- Index pour la table `event`
@@ -306,7 +242,7 @@ ALTER TABLE `event`
 -- Index pour la table `event_participant`
 --
 ALTER TABLE `event_participant`
-  ADD PRIMARY KEY (`event_id`,`participants_id`), ADD KEY `fk_event_user` (`event_id`), ADD KEY `fk_user_event` (`participants_id`);
+  ADD PRIMARY KEY (`event_id`,`user_id`), ADD KEY `fk_event_user` (`event_id`), ADD KEY `fk_user_event` (`user_id`);
 
 --
 -- Index pour la table `locality`
@@ -334,17 +270,17 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `activity`
 --
 ALTER TABLE `activity`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=33;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `event`
 --
 ALTER TABLE `event`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=29;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `locality`
 --
 ALTER TABLE `locality`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT pour la table `message`
 --
@@ -354,7 +290,24 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `activity`
+--
+ALTER TABLE `activity`
+ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `event_participant`
+--
+ALTER TABLE `event_participant`
+ADD CONSTRAINT `fk_event_user` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_user_event` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
